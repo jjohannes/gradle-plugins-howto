3293cdd4a63a591f6d7a8228a4013147bc2a148a
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-01-06 11:01:05 +0100

    Publish plugins
    Publishing is only an additional configuration of the plugins build.
    Hence it is the same setup for all approaches.
    You can publish:
    - To your own repository. Then you only need the
      id("maven-publish") plugin and configure your repository
    - To the Gradle Plugin Portal. Then you additionally need the
      id("com.gradle.plugin-publish") plugin and configure additional
      metadata that will be shown on the plugin portal website.

--- java/gradle-build-logic/java-plugins/build.gradle.kts
+++ java/gradle-build-logic/java-plugins/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java-gradle-plugin")
+    id("maven-publish") // if you never publish the plugin, you may remove this (but it also does not hurt)
+    id("com.gradle.plugin-publish") version "1.0.0" // if you do not publish to the Gradle Plugin Portal, you may remove this (but it also does not hurt)
 }
 
 group = "software.onepiece.gradle.pluginshowto"
@@ -24,4 +26,13 @@ dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.1.0") {
         because("Provides the 'com.diffplug.spotless' formatting plugin")
     }
+}
+
+// if you do not publish, the following is optional
+version = "1.0"
+publishing.repositories.maven("../../../_gradle-plugins-repository") // own repository to publish to (if needed) - run ':publish' task
+pluginBundle { // Plugin Portal Metadata (only needed if you publish to Gradle Plugin Portal) - run ':publishPlugins' task
+    website = "https://github.com/jjohannes/gradle-plugins-howto"
+    vcsUrl = "https://github.com/jjohannes/gradle-plugins-howto.git"
+    tags = listOf("example")
 }
--- scala/gradle-build-logic/java-plugins/build.gradle.kts
+++ scala/gradle-build-logic/java-plugins/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("java-gradle-plugin")
     id("scala")
+    id("maven-publish") // if you never publish the plugin, you may remove this (but it also does not hurt)
+    id("com.gradle.plugin-publish") version "1.0.0" // if you do not publish to the Gradle Plugin Portal, you may remove this (but it also does not hurt)
 }
 
 dependencies { implementation("org.scala-lang:scala-library:2.13.6") }
@@ -27,4 +29,13 @@ dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.1.0") {
         because("Provides the 'com.diffplug.spotless' formatting plugin")
     }
+}
+
+// if you do not publish, the following is optional
+version = "1.0"
+publishing.repositories.maven("../../../_gradle-plugins-repository") // own repository to publish to (if needed) - run ':publish' task
+pluginBundle { // Plugin Portal Metadata (only needed if you publish to Gradle Plugin Portal) - run ':publishPlugins' task
+    website = "https://github.com/jjohannes/gradle-plugins-howto"
+    vcsUrl = "https://github.com/jjohannes/gradle-plugins-howto.git"
+    tags = listOf("example")
 }
