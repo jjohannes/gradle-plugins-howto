d21bdce1941a97d8d07961c9b4bcce61b7feb2cb
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-01-06 11:03:01 +0100

    Add script to build all examples (for testing)

---
+++ build-all.sh
@@ -0,0 +1,27 @@
+cd clojure
+./gradlew build
+cd ..
+
+cd groovy
+./gradlew build
+cd ..
+
+cd groovy-dsl
+./gradlew build
+cd ..
+
+cd java
+./gradlew build
+cd ..
+
+cd kotlin
+./gradlew build
+cd ..
+
+cd kotlin-dsl
+./gradlew build
+cd ..
+
+cd scala
+./gradlew build
+cd ..
