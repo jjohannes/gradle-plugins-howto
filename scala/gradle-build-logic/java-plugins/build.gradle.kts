3eccbcbbd0120606c7a2532d34d24b833493e6e7
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-02-13 22:05:44 +0100

    Gradle 8 adjustments

--- scala/gradle-build-logic/java-plugins/build.gradle.kts
+++ scala/gradle-build-logic/java-plugins/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java-gradle-plugin")
     id("scala")
     id("maven-publish") // if you never publish the plugin, you may remove this (but it also does not hurt)
-    id("com.gradle.plugin-publish") version "1.0.0" // if you do not publish to the Gradle Plugin Portal, you may remove this (but it also does not hurt)
+    id("com.gradle.plugin-publish") version "1.1.0" // if you do not publish to the Gradle Plugin Portal, you may remove this (but it also does not hurt)
 }
 
 dependencies { implementation("org.scala-lang:scala-library:2.13.8") }
@@ -23,6 +23,13 @@ gradlePlugin {
         id = "${project.group}.java-application"
         implementationClass = "${project.group}.${name}"
     }
+    // -- Plugin Portal Metadata (only needed if you publish to Gradle Plugin Portal) - run ':publishPlugins' task
+    website.set("https://github.com/jjohannes/gradle-plugins-howto")
+    vcsUrl.set("https://github.com/jjohannes/gradle-plugins-howto.git")
+    plugins.all {
+        tags.set(listOf("example"))
+    }
+    // ---
 }
 
 dependencies {
@@ -34,8 +41,3 @@ dependencies {
 // if you do not publish, the following is optional
 version = "1.0"
 publishing.repositories.maven("../../../_gradle-plugins-repository") // own repository to publish to (if needed) - run ':publish' task
-pluginBundle { // Plugin Portal Metadata (only needed if you publish to Gradle Plugin Portal) - run ':publishPlugins' task
-    website = "https://github.com/jjohannes/gradle-plugins-howto"
-    vcsUrl = "https://github.com/jjohannes/gradle-plugins-howto.git"
-    tags = listOf("example")
-}
